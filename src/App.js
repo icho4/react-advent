fac136878b86c621231b1abc9829fc793be318df
icho4 <ian.cho4@gmail.com>
2020-11-18 12:13:31 -0500

    Remove ReadMe | add Styles | Buildout App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from "react";
 import {createGlobalStyle} from 'styled-components';
-import {createCalendar} from './helpers'
+import {StyledApp} from './AppStyles'
+import {createCalendar} from './helpers';
+import Hatch from './Hatch';
+
+
 
 function App() {
   const [hatches, setHatches] = useState(createCalendar());
-  console.log(hatches);
+
+  const handleFlipHatch = id => {
+    console.log(id)
+  }
 
   return (
     <>
-      Advent Calendar
+      {hatches.map(hatch => 
+        <Hatch 
+          key={hatch.id}
+          hatchData={hatch}
+          handleClick={handleFlipHatch}
+        />)}
     </>
   );
 }
---
+++ src/Hatch.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hatch = ({hatchData: {id, nr, text, img, open}, handleClick}) => (
+    <>
+
+        <div className={open ? "front open" : "front"}>
+            <p>{nr}</p>
+        </div>
+
+        <div className={open ? "back open" : "back"}>
+            <p>{text}</p>
+        </div>
+
+    </>
+
+
+)
+
+export default Hatch;
