6e993f2e30cfe4c708530aada24cc1712c82b06b
icho4 <ian.cho4@gmail.com>
2020-11-18 11:44:39 -0500

    Initial Scaffolding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {createGlobalStyle} from 'styled-components';
+import {createCalendar} from './helpers'
 
 function App() {
+  const [hatches, setHatches] = useState(createCalendar());
+  console.log(hatches);
 
   return (
     <>
